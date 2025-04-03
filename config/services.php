21ecd85e35b097e67eb8da9a83304de38aef6afc
SheenyKates <dagalasheenykates@gmail.com>
2025-04-03 18:07:32 +0800

    Update services.php

--- config/services.php
+++ config/services.php
@@ -3,11 +3,11 @@
 return [
     'users1' => [
         'base_uri' => env('USERS1_SERVICE_BASE_URL', 'http://localhost:8000'),
-        'endpoint' => env('USERS1_SERVICE_USERS_ENDPOINT', '/api/users1'), // Add this
+        'endpoint' => env('USERS1_SERVICE_USERS_ENDPOINT', '/api/users1'),
     ],
     
     'users2' => [
         'base_uri' => env('USERS2_SERVICE_BASE_URL', 'http://localhost:8001'),
-        'endpoint' => env('USERS2_SERVICE_USERS_ENDPOINT', '/api/users2'), // Add this
+        'endpoint' => env('USERS2_SERVICE_USERS_ENDPOINT', '/api/users2'), 
     ],
 ];
